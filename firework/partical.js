1f6d3f937f7e768d119a4aedaa17999db772c9cf
jq002 <1151176506@qq.com>
2018-03-01 19:32:07 +0800

    other demo

---
+++ firework/partical.js
@@ -0,0 +1,121 @@
+// ------------------------------
+// VARS
+// ------------------------------
+var a = document.getElementById('canvas'),
+    c = a.getContext('2d'),
+    w = a.width = window.innerWidth,
+    h = a.height = window.innerHeight,
+    area = w * h,
+    particleNum = 10,
+    ANIMATION;
+
+var particles = [];
+
+
+// ------------------------------
+// PARTICLE CONSTRUCTOR
+// ------------------------------
+function Particle(i) {
+    this.id = i;
+    this.hue = rand(90, 0, 1);
+    this.active = false;
+}
+
+Particle.prototype.build = function () {
+    this.x = w / 2;
+    this.y = h / 2;
+    this.r = rand(7, 2, 1);
+    this.vx = Math.random() * 10 - 5;
+    this.vy = Math.random() * 10 - 5;
+    this.gravity = .01;
+    // this.opacity = Math.random() + .5;
+    this.active = true;
+
+    // Draw circle with Canvas
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+    c.fillStyle = "hsla(" + this.hue + ",100%,50%,1)";
+    c.fill();
+};
+
+Particle.prototype.draw = function () {
+    // Set dynamic properties
+    this.active = true;
+    this.x += this.vx;
+    this.y += this.vy;
+    this.vy += this.gravity;
+    // this.hue -= 0.5;
+    this.r = Math.abs(this.r - .05);
+
+    // Draw circle with Canvas
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+    c.fillStyle = "hsla(" + this.hue + ",100%,50%,1)";
+    c.fill();
+
+    // Reset particle
+    if (this.r <= .05) {
+        this.active = false;
+    }
+};
+
+
+// ------------------------------
+// FUNCTIONALITY
+// ------------------------------
+function drawScene() {
+    c.fillStyle = "black";
+    c.fillRect(0, 0, w, h);
+
+    for (var i = 0; i < particles.length; i++) {
+        if (particles[i].active === true) {
+            particles[i].draw();
+        } else {
+            particles[i].build();
+        }
+    }
+
+    ANIMATION = requestAnimationFrame(drawScene);
+}
+
+function initCanvas() {
+    // var s = getComputedStyle(a);
+
+    if (particles.length) {
+        particles = [];
+        cancelAnimationFrame(ANIMATION);
+        ANIMATION;
+        console.log(ANIMATION);
+    }
+
+    w = a.width = window.innerWidth;
+    h = a.height = window.innerHeight;
+
+    for (var i = 0; i < particleNum; i++) {
+        particles.push(new Particle(i));
+    }
+
+    drawScene();
+    console.log(ANIMATION);
+}
+
+
+// ------------------------------
+// INIT
+// ------------------------------
+(function () {
+    initCanvas();
+    addEventListener('resize', initCanvas, false);
+})();
+
+
+// ------------------------------
+// HELPER FUNCTIONS
+// ------------------------------
+function rand(max, min, _int) {
+    var max = (max === 0 || max) ? max : 1,
+        min = min || 0,
+        gen = min + (max - min) * Math.random();
+
+    return (_int) ? Math.round(gen) : gen;
+};
