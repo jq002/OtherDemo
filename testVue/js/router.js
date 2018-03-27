72de16fe5dcaf00b1c906bc37bbb106132042ce8
jq002 <1151176506@qq.com>
2018-03-27 17:52:40 +0800

    about webpack vuex vue-router

--- testVue/js/router.js
+++ testVue/js/router.js
@@ -1,9 +1,35 @@
+const Foo = {
+    template: '<div> id:{{$route.params.id}}</div>'
+};
+const Bar = {
+    template: '<div> bar content<router-view></router-view></div>'
+};
+const Profile = {
+    template: '<div> profile content</div>'
+};
+const Your = {
+    template: '<div> your content</div>'
+};
+const routes = [{
+        path: '/foo/:id',
+        component: Foo
+    },
+    {
+        path: '/bar',
+        component: Bar,
+        children: [{
+            path: 'profile',
+            component:Profile
+        },{
+            path:'your',
+            component:Your
 
-const Foo={template:'<div> Foo content</div>'};
-const Bar={template:'<div> bar content</div>'};
-const routes=[
-    { path:'/foo',component: Foo},
-    { path:'/bar',component: Bar},
+        }]
+    },
 ];
-const router=new VueRouter({routes});
-const app=new Vue({router}).$mount('#app');
+const router = new VueRouter({
+    routes
+});
+const app = new Vue({
+    router
+}).$mount('#app');
