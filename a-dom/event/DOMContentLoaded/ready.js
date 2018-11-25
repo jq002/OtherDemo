857a154c2cc7b9bb342083532913e35386a135f8
jq002 <1151176506@qq.com>
2018-11-25 21:36:09 +0800

    HTML DOM Event

---
+++ a-dom/event/DOMContentLoaded/ready.js
@@ -0,0 +1,10 @@
+
+// (function(){
+//     for(let i=0;i<10000;i++){}
+// })()
+document.onreadystatechange=(e)=>{
+    console.log('readystate',document.readyState)
+    console.log(e)
+    console.log(e.currentTarget)
+}
+
---
+++ a-dom/event/DOMContentLoaded/ready2.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',(e)=>{
+    console.log('document DOMContentLoaded')
+    console.log(e)
+    // document.getElementById('title').style.marginBottom='100px';
+},false)
+window.addEventListener('DOMContentLoaded',(e)=>{
+    console.log('window DOMContentLoaded')
+    console.log(e)
+},false)
+
