6c92931dc64424a6fbdde957ec9aaaf52f35181e
jq002 <1151176506@qq.com>
2018-11-26 22:19:52 +0800

    DOMContentLoaded

--- a-dom/event/DOMContentLoaded/ready.js
+++ a-dom/event/DOMContentLoaded/ready.js
@@ -1,7 +1,7 @@
 
-// (function(){
-//     for(let i=0;i<10000;i++){}
-// })()
+(function(){
+    for(let i=0;i<10000;i++){}
+})()
 document.onreadystatechange=(e)=>{
     console.log('readystate',document.readyState)
     console.log(e)
